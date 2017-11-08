dad1fa59adf1035b0c44317447686875a4b85dc1
Tyler Stinson <tjstinso@gmail.com>
2017-11-08 12:12:55 -0800

    * cpu_loop: fixup

--- cond_variable.h
+++ cond_variable.h
@@ -13,9 +13,9 @@ typedef struct _Cond_Variable {
     FIFOq_p queue;
 } Cond_Variable_s;
 
-typedef Cond_Variable_s * c_Variable;
+typedef Cond_Variable_s * c_Variable_p;
 
-int cond_variable_signal(c_Variable var);
+int cond_variable_signal(c_Variable_p var);
 int cond_variable_wait(Lock_p lock, c_Variable var);
 
 #endif
